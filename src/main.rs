ed6ef62a78abf06d51f97beeef058cb21c5c47d7
lndexNull <nullablelndex@gmail.com>
2022-12-02 19:21:14 +0100

    implement path

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
+use std::io;
+
 mod experiment;
-pub mod days;
+mod days;
 
 fn main() {
-    experiment::experiment();
+
+    let cmp_str: String = String::from("experiment");
+    let mut path: String = String::new();
+
+    io::stdin()
+        .read_line(&mut path)
+        .unwrap();
+    
+
+    if cmp_str == path { //TODO figure out how to compare strings properly
+        println!("experiment  {}    {}", path, cmp_str);
+        experiment::experiment();
+    } else {
+        println!("days  {}    {}", path, cmp_str);
+        days::day_one::day1_task1();
+        days::day_one::day1_task2();
+    }
 }
